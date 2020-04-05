e2a8fd78e6a81d5ad07f4ec6592557949aea22a3
umisalamah <umis0179@outlook.com>
2020-04-05 15:17:03 +0700

    berhasil menampilkan semua data mahasiswa

---
+++ routes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(app) {
+    var jsonku = require('./controller');
+
+    app.route('/')
+        .get(jsonku.index);
+
+    app.route('/tampil')
+        .get(jsonku.tampilsemuamahasiswa); 
+
+ 
+    
+}
+
